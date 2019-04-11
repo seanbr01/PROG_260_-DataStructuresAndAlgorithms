3e42f0d4ca8ff40ac17f8e94f0fa6566ab7e040a
Sean Bruce <brusean@gmail.com>
2019-04-11 10:31:01 -0700

    1

---
+++ 002- Stacks/1-StacksUsingDotNet/UnitTestProjectStack/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnitTestProjectStack
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // verify that a newly created stack has no entries
+        [TestMethod]
+        public void IsInitialStackEmpty()
+        {
+            Stack TestStack = new Stack();
+            int expected = 3;
+            int actual = TestStack.Count;
+            Assert.AreEqual(expected, actual, "stack was not zero length");
+        }
+        
+        // varify peek works
+        [TestMethod]
+        public void PeakWorks()
+        {
+            Stack TestStack = new Stack();
+            for (int i = 0; i < 3; i++)
+            {
+                TestStack.Push(i);
+            }
+            int expected = 2;
+            int actual = (int)TestStack.Peek();
+            Assert.AreEqual(expected, actual, "are not equal");
+        }
+
+        // verify pop works after 3 pushes
+        [TestMethod]
+        public void PopsAfter3Pushes()
+        {
+            Stack TestStack = new Stack();
+        }
+    }
+}
