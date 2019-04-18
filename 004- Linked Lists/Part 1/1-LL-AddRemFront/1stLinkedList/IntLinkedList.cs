fbfbcb695e66e0c27b78c8f0a6afdddeacba17b8
Sean Bruce <brusean@gmail.com>
2019-04-18 10:29:03 -0700

    added 004 docs

---
+++ 004- Linked Lists/Part 1/1-LL-AddRemFront/1stLinkedList/IntLinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1stLinkedList
+{
+    public class IntLinkedList
+    {
+        //*********************************************************************************************
+        // define our Nested Class, a class that defines the objects that are in the linked lists
+        // could be private, but when you get into testing methodology, many times you will want child versions to use for testing.
+        protected class LinkedListNode
+        {
+            public int node_data;  // Property holds the "data", this could be a complex object, like a Customer object
+            public LinkedListNode node_next_pointer;  // Property holds the "pointer" (actually a node with a pointer in it)  to the next node in the list
+
+            // constructor: call it to create and set value of data, note the node_next_pointer property is set to null by default      
+            public LinkedListNode(int value)
+            {
+                node_data = value;
+            }
+        }
+        //*********************************************************************************************
+
+        // now define the IntLinkedList Class
+
+        // the only data item the class has is the pointer 
+        // (actually a node object with the NAME of another node object as a pointer in it)
+
+        protected LinkedListNode frontOfList; // No constructor, but an undefined object has the value null; which is our flag that the list is empty
+
+        // Method to add a node to the front of the list:
+        public void InsertAtFront(int value)
+        {
+            LinkedListNode newNode = new LinkedListNode(value);  // create a new node
+            newNode.node_next_pointer = frontOfList; // make this new first node point to what was just the first node;
+            // if the List had been empty, then we just made the newNode point to "null", which says there is no 2nd node.
+            frontOfList = newNode; // change the "reference" to front of list to point to this new one, which itself now points to the prior front of list
+            // we are dealing with this frontOfList because this method specifically says to add to the front.  Later we will insert into other places.
+        }
+
+        // Method to remove the node from the front of the list
+        public int RemoveFromFront()  // returns the "value" that the list object stores, in this case, an int
+        {
+            int returnVal; 
+            if (frontOfList == null)
+            {
+                throw new IndexOutOfRangeException("list is empty");
+            }
+            else
+            {
+                returnVal = frontOfList.node_data; // get the data from the node at the front of the list
+                frontOfList = frontOfList.node_next_pointer;  // copy the front of list's node's pointer to the next node, into the front of List
+                // if there was not another node, we just copied a null into the pointer, which says the list is now empty
+            }
+            return returnVal;
+        }
+
+    }
+}
---
+++ 004- Linked Lists/Part 1/1-LL-AddRemFront/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1stLinkedList;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+       [TestMethod]
+        // does LL hold data correctly, and in the correct order
+        // insert 3 values at top, remove 2 from top, total should be = val of the last two added
+        public void StoreAndRetrieveFromTop()
+        {
+            IntLinkedList myLL = new IntLinkedList();
+            int total = 0;
+            myLL.InsertAtFront(111);
+            myLL.InsertAtFront(222);
+            myLL.InsertAtFront(333);
+            total = total + myLL.RemoveFromFront();
+            total = total + myLL.RemoveFromFront();
+            Assert.AreEqual<int>(555, total);
+        }
+
+        [TestMethod]
+        // try and remove from top from an empty LL
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void RemoveFromEmptyLL()
+        {
+            IntLinkedList myLL = new IntLinkedList();
+            myLL.RemoveFromFront();
+        }
+
+        [TestMethod]
+        // Add entries, remove entries, make sure LL says its empty
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void IsEmptyLLafterAddAndRemove()
+        {
+            IntLinkedList myLL = new IntLinkedList();
+            myLL.InsertAtFront(111);
+            myLL.InsertAtFront(222);
+            myLL.RemoveFromFront();
+            myLL.RemoveFromFront();
+            myLL.RemoveFromFront();
+        }
+
+        [TestMethod]
+        // does LL store negative numbers
+        public void StoreNegativeNumbers()
+        {
+            IntLinkedList myLL = new IntLinkedList();
+            int total = 0;
+            myLL.InsertAtFront(-111);
+            myLL.InsertAtFront(-222);
+            total = total + myLL.RemoveFromFront();
+            total = total + myLL.RemoveFromFront();
+            Assert.AreEqual<int>(-333, total);
+        }
+
+        [TestMethod]
+        // does LL store largest int32
+        public void StoreLargestInt32()
+        {
+            IntLinkedList myLL = new IntLinkedList();
+            myLL.InsertAtFront(Int32.MaxValue);
+            int actual =  myLL.RemoveFromFront();
+            Assert.AreEqual<int>(2147483647, actual);
+        }
+    }
+}
