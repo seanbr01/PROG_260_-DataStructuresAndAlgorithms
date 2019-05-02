bb8c78271850b77ce172f39b6bfdd70afedf34a7
Sean Bruce <brusean@gmail.com>
2019-05-02 10:07:30 -0700

    005 Recursion added

---
+++ 005- recursion/1st-Recursion/FirstRecursion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintClass myPrintClass = new PrintClass();  // instantiate an instance of the class
+            int endResult = myPrintClass.recursionDemo(3);  // call the recurives method the first time from Main
+            Console.WriteLine();
+            Console.WriteLine("End result: " + endResult);  // write out the value retuned
+            Console.ReadLine();
+        }
+    }
+
+
+    class PrintClass
+    {
+        public int recursionDemo(int f)
+        {
+            int result = 0;
+            Console.WriteLine("Handed " + f);  // always write a line out before calling itself
+            if (f <= 0)   // recursive code needs a way to come to an end of the recursion, this one counts down the value passed in
+                return 10;  // I picked 10 just to make this counting distinct from the counting down of f
+                // critcal thing to understand, where is the above return statement returning to???
+            else
+            {
+                f = f - 1;  // this is key, this is where we are counting down the number of times to do the recursion
+                result = recursionDemo(f);  // calling >> ourself <<  this is what makes it "recursion"
+                // it keeps getting delayed (put on the call stack), as we jump away to another copy of ourself
+                // when we hit the bottom of the recursion, we will "unwind" our way back up, and do the WriteLine 3 times
+
+                // these lines will not execute the first time we call recursionDemo,
+                result = result + 1;  // this is just a way for us to see the order of how things happen.
+                              
+                // the first Console.WriteLine gets called 4 times, 
+                // but only when the recursion gets to its limit will this bottom Console.WriteLine finally execute 4 times
+                Console.WriteLine("Returning " + result);  
+
+                return result;
+            }
+
+        }
+    }
+
+   
+
+}
---
+++ 005- recursion/3rd-ConWriteAssendDesc/PrintAssendNumbersRec/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAssendNumbersRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // program will  write out numbers from 1 to max using a recursive method
+            // often when you get your recursion scheme wrong, it will call itself forever, 
+            // and then you will get this error msg:  "Process is terminated due to StackOverflowException."
+            
+            Console.WriteLine("first do the count up");
+            Console.WriteLine();
+            Console.Write("enter the value you want to count from 1 up to:  ");
+            int max = Convert.ToInt16(Console.ReadLine());
+            CountUp myCountUp = new CountUp();
+            myCountUp.CounterUp(max); 
+
+            // now it will count down
+            Console.WriteLine();
+            Console.WriteLine("then do the count down");
+            CountDown myCountDown = new CountDown();
+            myCountDown.CounterDown(max);
+
+            
+           
+            Console.ReadLine();
+        }
+    }
+
+    class CountUp
+    {
+        public void CounterUp(int value) // recurse, then do the work
+        {
+            int myCount = value;
+            if (myCount < 1) // handles the case where the user passed in a zero or negative number
+                // but also, this will terminate the recursion
+            {
+               return;
+            }
+            else
+            {
+                CounterUp(--myCount); // call myself  as count down 4,3,2,1,0
+            }
+            // here we "do the work", after coming back from each return
+            Console.WriteLine("Next number is: {0}", myCount +1);  // since we decrement (--Count) before we get to the WriteLine, my printable value is off by 1
+            return;
+
+        }
+    }
+
+    class CountDown
+    {
+        public void CounterDown(int value) // do the work, then recurse
+        {
+            int myCount = value;
+            Console.WriteLine("Next number is: {0}", myCount);   // notice we do "the work" before the recursive call
+            if (myCount <= 1) // handles the case where the user passed in a zero or negative number
+            // but also, this will terminate the recursion
+            {
+                return;
+            }
+            else
+            {
+                CounterDown(--myCount); // call myself  as count down 4,3,2,1,0
+            }
+            //just pop the returns
+           // return;
+
+        }
+    }
+  
+}
