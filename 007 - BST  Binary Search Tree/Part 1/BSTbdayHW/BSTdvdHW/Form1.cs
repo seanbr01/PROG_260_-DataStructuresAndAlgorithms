d6512e3d20570eb7dc11e6ce643e95364d8758ca
Sean Bruce <brusean@gmail.com>
2019-05-21 10:31:22 -0700

    Week 8 Docs

---
+++ 007 - BST  Binary Search Tree/Part 1/BSTbdayHW/BSTdvdHW/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Birthday
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bday_BST myBST = new Bday_BST();
+
+        private void buttonAddNew_Click(object sender, EventArgs e)
+        {
+            Birthday newBirthday = new Birthday();
+            newBirthday.Date = Convert.ToInt32(textBoxBday.Text);
+            newBirthday.Name = textBoxName.Text;
+            newBirthday.Ideas = textBoxIdeas.Text;
+            myBST.AddRec(newBirthday);  // showing error since Bday_BST code does not
+            // currently accept object of type Birthday
+
+        }
+
+        private void buttonLookUp_Click(object sender, EventArgs e)
+        {
+            Birthday existingBirthday;
+            existingBirthday = myBST.FindRec(Convert.ToInt32(textBoxLookUpDate.Text));
+            // showing error since Bday_BST code does not return a Birthday object
+            textBoxName.Text = existingBirthday.Name;
+            textBoxIdeas.Text = existingBirthday.Ideas;
+
+        }
+
+    }
+}
