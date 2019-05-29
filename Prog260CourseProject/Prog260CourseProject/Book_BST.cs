a88eb9628ebe7bd83f45d174aa8ee002de8e6cf9
Sean Bruce <seanbruce@hotmail.com>
2019-05-29 13:49:39 -0700

    Work on Class Project

---
+++ Prog260CourseProject/Prog260CourseProject/Book_BST.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog260CourseProject
+{
+    class Book_BST
+    {
+        protected BSTnode bstTop;
+        
+        protected class BSTnode
+        {
+            public Book BookObject;
+            public BSTnode LeftNode;
+            public BSTnode RightNode;
+
+            public BSTnode(Book key)
+            {
+                BookObject = key;
+            }
+            
+            public void AddRec_BSTnode(Book keyParam)
+            {
+                if (keyParam == this.BookObject)
+                {
+                    throw new ApplicationException("Msg from BSTnode class: tried to add a duplicate value");
+                }
+
+                if (keyParam.ISBN < this.BookObject.ISBN)
+                {
+                    if (this.LeftNode == null)
+                    {
+                        BSTnode newBSTnode = new BSTnode(keyParam);
+                        this.LeftNode = newBSTnode;
+                        return;
+                    }
+                    else
+                    {
+                        this.LeftNode.AddRec_BSTnode(keyParam);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (this.RightNode == null)
+                    {
+                        BSTnode newBSTnode = new BSTnode(keyParam);
+                        this.RightNode = newBSTnode;
+                        return;
+                    }
+                    else
+                    {
+                        this.RightNode.AddRec_BSTnode(keyParam);
+                        return;
+                    }
+                }
+            }
+            
+            public Book FindRec_BSTnode(int target)
+            {
+                BSTnode current = this;
+
+                if (target == current.BookObject.ISBN)
+                {
+                    return current.BookObject;
+                }
+                else if (target > current.BookObject.ISBN)
+                {
+                    if (current.RightNode == null)
+                    {
+                        throw new ApplicationException("no such ISBN");
+                    }
+                    else
+                    {
+                        current = current.RightNode;
+                        return current.FindRec_BSTnode(target);
+                    }
+
+                }
+                else
+                {
+                    if (current.LeftNode == null)
+                    {
+                        throw new ApplicationException("no such ISBN");
+                    }
+                    else
+                    {
+                        current = current.LeftNode;
+                        return current.FindRec_BSTnode(target);
+                    }
+                }
+            }
+        }
+
+        public void AddRec(Book keyParam)
+        {
+            if (bstTop == null)
+            {
+                bstTop = new BSTnode(keyParam);
+                return;
+            }
+            else
+            {
+                bstTop.AddRec_BSTnode(keyParam);
+            }
+        }
+        
+        public Book FindRec(int target)
+        {
+            if (bstTop == null)
+            {
+                throw new ApplicationException("no such ISBN"); ;
+            }
+            else
+            {
+                return bstTop.FindRec_BSTnode(target);
+            }
+        }
+
+        public List<Book> GetAll()
+        {
+            List<Book> books = new List<Book>();
+            GetAllRecr(bstTop, books);
+            return books;
+        }
+
+        private void GetAllRecr(BSTnode current, List<Book> books)
+        {
+            if (current == null)
+            {
+                return;
+            }
+
+            GetAllRecr(current.LeftNode, books);
+            books.Add(current.BookObject);
+            GetAllRecr(current.RightNode, books);
+        }
+    }
+}
--- Prog260CourseProject/Prog260CourseProject/Form1.cs
+++ Prog260CourseProject/Prog260CourseProject/Form1.cs
@@ -12,19 +12,41 @@ namespace Prog260CourseProject
 {
     public partial class Form1 : Form
     {
+        Book_BST myBook_BST;
         public Form1()
         {
             InitializeComponent();
+            myBook_BST = new Book_BST();
         }
 
         private void btnAddNewBook_Click(object sender, EventArgs e)
         {
-
+            Book book = new Book()
+            {
+                ISBN = Convert.ToInt32(txbISBN.Text),
+                Title = txbTitle.Text,
+                Author = txbAuthor.Text,
+                Rating = Convert.ToInt32(txbRating.Text),
+                PubYear = Convert.ToInt32(txbYear.Text)
+            };
+            myBook_BST.AddRec(book);
+            clearForm();
         }
 
         private void btnFindFromISBN_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Book book = myBook_BST.FindRec(Convert.ToInt32(txbISBN.Text));
+                txbTitle.Text = book.Title;
+                txbAuthor.Text = book.Author;
+                txbRating.Text = book.Rating.ToString();
+                txbYear.Text = book.PubYear.ToString();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnRemoveFromISBN_Click(object sender, EventArgs e)
@@ -34,7 +56,16 @@ namespace Prog260CourseProject
 
         private void btnGetAllISBNs_Click(object sender, EventArgs e)
         {
+            dgvISBN.DataSource = myBook_BST.GetAll();
+        }
 
+        private void clearForm()
+        {
+            txbISBN.Text = string.Empty;
+            txbTitle.Text = string.Empty;
+            txbAuthor.Text = string.Empty;
+            txbRating.Text = string.Empty;
+            txbYear.Text = string.Empty;
         }
     }
 }
